47d81aa86acd66acbf4f96dbdd1662277d786e11
gracekyalo <gracendanu27@gmail.com>
2022-12-06 16:33:51 +0300

    Fallback controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,38 +1,38 @@
-class ReviewsController < ApplicationController
-    def index
-        reviews = Review.all
-        render json: reviews, status: :ok
-      end
+# class ReviewsController < ApplicationController
+#     def index
+#         reviews = Review.all
+#         render json: reviews, status: :ok
+#       end
     
-      def show
-        review = Review.find(params[:id])
-        render json: review, status: :ok
-      end
+#       def show
+#         review = Review.find(params[:id])
+#         render json: review, status: :ok
+#       end
     
-      def create
-        render json: Review.create!(review_params), status: :created
-      end
+#       def create
+#         render json: Review.create!(review_params), status: :created
+#       end
     
-      def update
-        review = Review.find(params[:id])
-        render json: review.update!(review_params), status: :created
-      end
+#       def update
+#         review = Review.find(params[:id])
+#         render json: review.update!(review_params), status: :created
+#       end
     
-      def destroy
-        review = Review.find(params[:id])
-        review.destroy
-        head :no_content
-      end
+#       def destroy
+#         review = Review.find(params[:id])
+#         review.destroy
+#         head :no_content
+#       end
     
-      def user_reviews
-        current_user_reviews = Review.joins(:user).where(:user => {:id => params[:id]})
-        render json: current_user_reviews
-      end
+#       def user_reviews
+#         current_user_reviews = Review.joins(:user).where(:user => {:id => params[:id]})
+#         render json: current_user_reviews
+#       end
     
-      private
+#       private
     
-      def review_params
-        params.permit(:content, :user_id, :destination_id)
-      end
+#       def review_params
+#         params.permit(:content, :user_id, :destination_id)
+#       end
     
-end
+# end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
     has_many :visits 
     has_many :destinations, through: :visits
     has_many :reviews
+
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
+    validates :name, presence: true
+    validates :email, presence: true
 end
