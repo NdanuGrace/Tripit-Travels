5267cddc9f85ff943078532c4c2cb9f8d46893f4
gracekyalo <gracendanu27@gmail.com>
2022-12-09 22:54:00 +0300

    my visits update

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,6 @@ import LoginSignupPage from "./Components/LoginSignupPage";
 function App() {
   const [user, setUser] = useState(null);
   const [reviews, setReviews] = useState([])
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
   const [destinations, setDestinations] = useState([])
 
 
@@ -26,12 +25,16 @@ function App() {
       }
     });
   }, []);
+  useEffect(() => {
+    fetch("/destinations").then((response) => {
+      if (response.ok) {
+        response.json().then((data) => setDestinations(data));
+      }
+    });
+  }, []);
 
-  const unlockDestinations = () => {
-    fetch('/destinations')
-    .then(r => r.json())
-    .then(data => setDestinations(data))
-    }
+ 
+  
   const unlockReviews = () => {
     fetch(`reviews`)
       .then((res) => res.json())
@@ -58,7 +61,7 @@ function App() {
           <Route exact path="/destinationprofile" element={<DestinationProfile/>}></Route>
 
           <Route  path="/myreviews" element={<MyReviews user={user} reviews={reviews} setReviews={setReviews} destinations={destinations} />}></Route>
-          <Route  path="/myvisits" element={<MyVisits />}></Route>
+          <Route  path="/myvisits" element={<MyVisits user={user} destinations={destinations} />}></Route>
           <Route  path="/userprofile" element={<UserProfile user={user} />} ></Route>
           <Route  path="/availabledestinations" element={<AvailableDestinations />}></Route>
 
--- client/src/Components/VisitCard.js
+++ client/src/Components/VisitCard.js
@@ -1,30 +1,36 @@
 import React from 'react';
 
-function VisitCard({ start_date, end_date }) {
-    // const destinationVisit = destinations.map(destination => destination === destination)
+function VisitCard({ start_date, end_date, destinations }) {
 
     return (
         <div className='visit-outer-div'>
-            {/* <div className='visit-card' style={{backgroundImage: `url(${destinationVisit[0].image}})`}}>
-              
-                 <div className='visit-information-container'>
-                    <div className='visit-info-div'>
-                        <h2 className='visit-title'>{destinationVisit[0].location}</h2>  */}
-                        <div className='visit-more-information'>
-                            <div className='from-and-to-container'>
-                                <div className='box from'>
-                                    <p className='from'>From: {start_date}</p>
-                                </div>
-                                <div className='box to'>
-                                    <p className='to'>To: {end_date}</p>
+            {destinations.map((destination) => (
+
+                <div className='visit-card' style={{ backgroundImage: `url(${destination.image}})` }}>
+
+                    <div className='visit-information-container'>
+                        <div className='visit-info-div'>
+                            <h2 className='visit-title'>{destination.location}</h2>
+                            <div className='visit-more-information'>
+                                <div className='from-and-to-container'>
+                                    <div className='box from'>
+                                        <p className='from'>From: {start_date}</p>
+                                    </div>
+                                    <div className='box to'>
+                                        <p className='to'>To: {end_date}</p>
+
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                // </div>
-            // </div>
-        // </div>
-    )
-}
+                </div>
+            
+    
+    ))}
+
+    </div>
+    )  }      
+
 
 export default VisitCard;
